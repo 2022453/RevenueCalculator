fdc64fedaff2fa0b2fefc1b830b921caf864b5ef
2022453 <2022453@student.cct.ie>
2023-12-12 16:24:21 +0000

    Create Users.java
    Created the users class with the proper attributes.

---
+++ src/revenuecalculator/Users.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package revenuecalculator;
+
+/**
+ *
+ * @author paulooliveira
+ */
+public class Users {
+    
+    
+    // created the atributes necessary for our users. 
+    
+    private static int user_id = 1;
+    private int userID;
+    private String first_name;
+    private String surname_name;
+    private String day_of_birthday;
+    private String address;
+    private String UserName;
+    private String Password;
+    private double gross_income;
+    
+}
