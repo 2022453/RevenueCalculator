2cf2f151340267e2187627df7cf2bd92485829d0
Diogolemoos <146637354+Diogolemoos@users.noreply.github.com>
2023-12-19 23:04:03 +0000

    Update RevenueCalculator.java
    Fixing errors on the main class

--- src/revenuecalculator/RevenueCalculator.java
+++ src/revenuecalculator/RevenueCalculator.java
@@ -4,10 +4,7 @@
  */
 package revenuecalculator;
 
-<<<<<<< HEAD
 import java.sql.SQLException;
-=======
->>>>>>> 2a1974af45d4a4eba7bcf5849ca19502d445b996
 import java.util.ArrayList;
 import java.util.Scanner;
 import static revenuecalculator.DatabaseReader.getIncomeFromDatabase;
@@ -21,10 +18,7 @@ public class RevenueCalculator {
     /**
      * @param args the command line arguments
      */
-<<<<<<< HEAD
-=======
 
->>>>>>> 2a1974af45d4a4eba7bcf5849ca19502d445b996
     public static void main(String[] args) throws SQLException {
         
 
@@ -101,15 +95,15 @@ boolean adminLoginSuccessful = false;
             while (true) {
                 System.out.println("Admin Menu:");
                 System.out.println("1. Modify Admin Information");
-<<<<<<< HEAD
+
                 System.out.println("2. see list");
                 System.out.println("3. Remove User");
                 System.out.println("4. Track User Activity");
-=======
+
                 System.out.println("2. See list of Users");
                 System.out.println("3. Remove Users");
                 System.out.println("4. Track User's Activity");
->>>>>>> 2a1974af45d4a4eba7bcf5849ca19502d445b996
+
                 System.out.println("5. Exit Admin Menu");
 
                 int adminChoice = myKB.nextInt();
@@ -496,10 +490,9 @@ UserActivityLogger.logActivity(enteredUserName, "Modified user information");
     }
     
 
-<<<<<<< HEAD
-=======
+
     // to check if contains number when users is signing up his name and surname
->>>>>>> 2a1974af45d4a4eba7bcf5849ca19502d445b996
+
     private static boolean containsDigit(String s) {
         for (char c : s.toCharArray()) {
             if (Character.isDigit(c)) {
@@ -514,9 +507,5 @@ UserActivityLogger.logActivity(enteredUserName, "Modified user information");
     String regex = "^(\\d{4})[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12][0-9]|3[01])$";
     return date.matches(regex); 
   
-     } 
-<<<<<<< HEAD
-   }
-=======
-   }
->>>>>>> 2a1974af45d4a4eba7bcf5849ca19502d445b996
+    }
+}
